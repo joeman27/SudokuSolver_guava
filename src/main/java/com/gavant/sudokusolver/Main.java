253a3fef1d69e2224f8802fc494d176d6785f34b
Joe <jmankovecky21@gmail.com>
2022-06-22 00:59:24 -0400

    Initial commit

---
+++ src/main/java/com/gavant/sudokusolver/Main.java
@@ -0,0 +1,20 @@
+package com.gavant.sudokusolver;
+
+import com.gavant.sudokusolver.SudokuSolver;
+
+/**
+ * Hello world!
+ */
+public final class Main {
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    private static void readFile(){}
+
+    private static void verifyPuzzle(){}
+}
